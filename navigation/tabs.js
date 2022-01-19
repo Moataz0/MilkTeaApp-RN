be04f5a418204e31130372858fb2ff01511b560d
unknown <moataz.mohamed@brmaja.com>
2022-01-19 12:47:05 +0200

    Update tab file

---
+++ navigation/tabs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Tabs = () => {
+    return (
+        <View>
+            <Text>Tabs</Text>
+        </View>
+    )
+}
+
+export default Tabs
+
+const styles = StyleSheet.create({})
